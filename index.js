203a5f31991999ff44dcba75d30d5a290c33ad3a
Jess Pesale <jess_pesale@aol.com>
2021-09-06 19:15:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json))
+  // .then(json => console.log(json))
 }
 
 function renderBooks(books) {
@@ -8,10 +11,17 @@ function renderBooks(books) {
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
+    const h3 = document.createElement('h3')
+    h3.innerHTML = "Author: " + book.authors[0]
+    const link = document.createElement("h4")
+    link.innerHTML = book.url
     main.appendChild(h2);
+   main.appendChild(h3);
+    main.appendChild(link);
   });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
